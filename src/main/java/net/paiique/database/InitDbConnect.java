1c04cdc46c61a8ddf535c89a3414af24e0f4e2f9
paique <bielperrett20@gmail.com>
2023-11-29 18:28:28 -0300

    Frist commit

--- src/main/java/net/paiique/database/InitDbConnect.java
+++ src/main/java/net/paiique/database/InitDbConnect.java
@@ -4,10 +4,9 @@ import java.sql.*;
 import static net.paiique.util.Env.*;
 
 public class InitDbConnect {
-            private String jdbc = "jdbc:mysql://"+ DB_HOST.get() +":"+ DB_PORT.get() +"/" + DB_NAME.get();
-            private Connection con;
+    private final Connection con;
     public InitDbConnect() {
-        System.out.println(jdbc);
+        String jdbc = "jdbc:mysql://" + DB_HOST.get() + ":" + DB_PORT.get() + "/" + DB_NAME.get();
         System.out.println("🛜 | Connecting to database " + DB_NAME.get() + " on " + DB_HOST.get() + ":" + DB_PORT.get());
         try {
             con = DriverManager.getConnection(jdbc, DB_USER.get(), DB_PASS.get());
